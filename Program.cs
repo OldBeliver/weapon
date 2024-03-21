cf8405d81a8c8141206a1da5091d294f94193f15
OldBeliver <380506338004@yandex.com>
2024-03-21 10:50:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weapon
+{
+    class Weapon
+    {
+        private readonly int _damage;
+        private int _bullets;
+
+        public void Fire(Player player)
+        {
+            if (_bullets > 0)
+            {
+                _bullets--;
+                player.TakeDamage(_damage);
+            }
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+                return;
+
+            if (damage >= _health)
+                _health = 0;
+            else
+                _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            if (player != null)
+                _weapon.Fire(player);
+        }
+    }
+}
