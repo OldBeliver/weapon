28815ff9ebd4d3e7c85b0f2c7b823b47bfbeb8b2
OldBeliver <380506338004@yandex.com>
2024-03-23 10:55:28 +0300

    ошибки от ментора

--- Program.cs
+++ Program.cs
@@ -2,7 +2,31 @@
 
 namespace weapon
 {
-     class Weapon
+    /*
+     * 
+     *  
+        1.   public void Fire(Player player) - в каждом публичном методе нужна проверка аргументов  на null/outOfRange и бросать исключение в случае ошибки
+        
+        2.   _bullets > 0 - это лучше вынести в свойство
+        
+        3.   if (damage < 0)
+                return; - нужно бросать исключение, это исключительная ситуация когда урон отрицательный
+        
+        4.   if (damage >= _health)
+                _health = 0;
+            else
+                _health -= damage; - лучше использовать Math.Max
+        
+        5.   if (player != null)
+                _weapon.Fire(player); - тут так же будет тихо глючить если мы передадим null, нужно исключние
+        
+        6. Для всех классов нужны конструкторы с проверкой аргументов на null/outOfRange
+    *
+    */
+
+    
+
+    class Weapon
     {
         private readonly int _damage;
         private int _bullets;
