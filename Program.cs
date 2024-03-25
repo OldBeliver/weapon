eb3d5ec517c6a122e4167b5ff89566b97694cf44
OldBeliver <380506338004@yandex.com>
2024-03-25 16:56:04 +0300

    подготовка к проверке

--- Program.cs
+++ Program.cs
@@ -1,39 +1,4 @@
 ﻿using System;
-/*
- * 
- * 
-1.   public void Fire(Player player) - тут нужна проверка player на null
-
-2. HaveBullets - лучше именовать CanFire
-
-3.          if (health < 0)
-            {
-                throw new ArgumentOutOfRangeException($"{_health} уже не то");
-            } - придерживайся одного стиля, либо со скобками, либо без
-
-4.             if (HaveBullets)
-            { - лучше инвертировать if
-
- * 
- * 
- * 
-1.         public void Fire(Player player) - в каждом публичном методе нужна проверка аргументов  на null/outOfRange и бросать исключение в случае ошибки
-
-2. _bullets > 0 - это лучше вынести в свойство
-
-3.             if (damage < 0)
-                return; - нужно бросать исключение, это исключительная ситуация когда урон отрицательный
-
-4.             if (damage >= _health)
-                _health = 0;
-            else
-                _health -= damage; - лучше использовать Math.Max
-
-5.             if (player != null)
-                _weapon.Fire(player); - тут так же будет тихо глючить если мы передадим null, нужно исключние
-
-6. Для всех классов нужны конструкторы с проверкой аргументов на null/outOfRange
- */
 
 namespace weapon
 {
@@ -60,7 +25,7 @@ namespace weapon
         public void Fire(Player player)
         {
             if (player is null)
-                throw new ArgumentNullException($"цель {nameof(player)} потеряна");
+                throw new ArgumentNullException($"цель {nameof(player)} не объявлена");
 
             if (CanNotFire)
                 throw new ArgumentOutOfRangeException(nameof(Bullets));
@@ -79,7 +44,6 @@ namespace weapon
         {
             if (health < 0)
                 throw new ArgumentOutOfRangeException($"{_health} уже не то");
-
 
             _health = health;
         }
